80cb2c6c0ef3889c61c1aa2b596e283c45ea2e6f
atomatic <mail@martin-lihs.com>
2021-10-29 01:50:28 +0200

    Update and new Functions

---
+++ src/brandColors.js
@@ -0,0 +1,686 @@
+const colors = [
+				{
+					"name": "DB Red",
+					"group": "Primary Brand Colors",
+					"color": "#ec0016"
+				},
+				{
+					"name": "DB White",
+					"group": "Primary Brand Colors",
+					"color": "#ffffff"
+				},
+				{
+					"name": "DB Yellow 100",
+					"group": "Brand Colors",
+					"color": "#ffffdc"
+				},
+				{
+					"name": "DB Yellow 200",
+					"group": "Brand Colors",
+					"color": "#ffffaf"
+				},
+				{
+					"name": "DB Yellow 300",
+					"group": "Brand Colors",
+					"color": "#fff876"
+				},
+				{
+					"name": "DB Yellow 400",
+					"group": "Brand Colors",
+					"color": "#fff000"
+				},
+				{
+					"name": "DB Yellow 500",
+					"group": "Brand Colors",
+					"color": "#ffd800"
+				},
+				{
+					"name": "DB Yellow 600",
+					"group": "Brand Colors",
+					"color": "#ffbb00"
+				},
+				{
+					"name": "DB Yellow 700",
+					"group": "Brand Colors",
+					"color": "#ff9b00"
+				},
+				{
+					"name": "DB Yellow 800",
+					"group": "Brand Colors",
+					"color": "#ff7a00"
+				},
+				{
+					"name": "DB Orange 100",
+					"group": "Brand Colors",
+					"color": "#fff4d8"
+				},
+				{
+					"name": "DB Orange 200",
+					"group": "Brand Colors",
+					"color": "#fce3b4"
+				},
+				{
+					"name": "DB Orange 300",
+					"group": "Brand Colors",
+					"color": "#faca7f"
+				},
+				{
+					"name": "DB Orange 400",
+					"group": "Brand Colors",
+					"color": "#f8ab37"
+				},
+				{
+					"name": "DB Orange 500",
+					"group": "Brand Colors",
+					"color": "#f39200"
+				},
+				{
+					"name": "DB Orange 600",
+					"group": "Brand Colors",
+					"color": "#d77b00"
+				},
+				{
+					"name": "DB Orange 700",
+					"group": "Brand Colors",
+					"color": "#c05e00"
+				},
+				{
+					"name": "DB Orange 800",
+					"group": "Brand Colors",
+					"color": "#a24800"
+				},
+				{
+					"name": "DB Red 100",
+					"group": "Brand Colors",
+					"color": "#fee6e6"
+				},
+				{
+					"name": "DB Red 200",
+					"group": "Brand Colors",
+					"color": "#fcc8c3"
+				},
+				{
+					"name": "DB Red 300",
+					"group": "Brand Colors",
+					"color": "#fa9090"
+				},
+				{
+					"name": "DB Red 400",
+					"group": "Brand Colors",
+					"color": "#f75056"
+				},
+				{
+					"name": "DB Red 500",
+					"group": "Brand Colors",
+					"color": "#ec0016"
+				},
+				{
+					"name": "DB Red 600 ",
+					"group": "Brand Colors",
+					"color": "#c50014"
+				},
+				{
+					"name": "DB Red 700 ",
+					"group": "Brand Colors",
+					"color": "#9b000e"
+				},
+				{
+					"name": "DB Red 800",
+					"group": "Brand Colors",
+					"color": "#740009"
+				},
+				{
+					"name": "DB Burgundy 100",
+					"group": "Brand Colors",
+					"color": "#f4e8ed"
+				},
+				{
+					"name": "DB Burgundy 200",
+					"group": "Brand Colors",
+					"color": "#edcbd6"
+				},
+				{
+					"name": "DB Burgundy 300",
+					"group": "Brand Colors",
+					"color": "#da9aa8"
+				},
+				{
+					"name": "DB Burgundy 400",
+					"group": "Brand Colors",
+					"color": "#c0687b"
+				},
+				{
+					"name": "DB Burgundy 500",
+					"group": "Brand Colors",
+					"color": "#a9455d"
+				},
+				{
+					"name": "DB Burgundy 600",
+					"group": "Brand Colors",
+					"color": "#8c2e46"
+				},
+				{
+					"name": "DB Burgundy 700",
+					"group": "Brand Colors",
+					"color": "#641e32"
+				},
+				{
+					"name": "DB Burgundy 800",
+					"group": "Brand Colors",
+					"color": "#4d0820"
+				},
+				{
+					"name": "DB Pink 100",
+					"group": "Brand Colors",
+					"color": "#fdeef8"
+				},
+				{
+					"name": "DB Pink 200",
+					"group": "Brand Colors",
+					"color": "#f9d2e5"
+				},
+				{
+					"name": "DB Pink 300",
+					"group": "Brand Colors",
+					"color": "#f4aece"
+				},
+				{
+					"name": "DB Pink 400",
+					"group": "Brand Colors",
+					"color": "#ee7bae"
+				},
+				{
+					"name": "DB Pink 500",
+					"group": "Brand Colors",
+					"color": "#e93e8f"
+				},
+				{
+					"name": "DB Pink 600",
+					"group": "Brand Colors",
+					"color": "#db0078"
+				},
+				{
+					"name": "DB Pink 700",
+					"group": "Brand Colors",
+					"color": "#b80065"
+				},
+				{
+					"name": "DB Pink 800",
+					"group": "Brand Colors",
+					"color": "#970052"
+				},
+				{
+					"name": "DB Violett 100",
+					"group": "Brand Colors",
+					"color": "#f4eefa"
+				},
+				{
+					"name": "DB Violett 200",
+					"group": "Brand Colors",
+					"color": "#e0cde4"
+				},
+				{
+					"name": "DB Violett 300",
+					"group": "Brand Colors",
+					"color": "#c2a1c7"
+				},
+				{
+					"name": "DB Violett 400",
+					"group": "Brand Colors",
+					"color": "#9a6ca6"
+				},
+				{
+					"name": "DB Violett 500",
+					"group": "Brand Colors",
+					"color": "#814997"
+				},
+				{
+					"name": "DB Violett 600",
+					"group": "Brand Colors",
+					"color": "#6e368c"
+				},
+				{
+					"name": "DB Violett 700",
+					"group": "Brand Colors",
+					"color": "#581d70"
+				},
+				{
+					"name": "DB Violett 800",
+					"group": "Brand Colors",
+					"color": "#421857"
+				},
+				{
+					"name": "DB Blue 100",
+					"group": "Brand Colors",
+					"color": "#e0effb"
+				},
+				{
+					"name": "DB Blue 200",
+					"group": "Brand Colors",
+					"color": "#b4d5f6"
+				},
+				{
+					"name": "DB Blue 300",
+					"group": "Brand Colors",
+					"color": "#73aef4"
+				},
+				{
+					"name": "DB Blue 400",
+					"group": "Brand Colors",
+					"color": "#347de0"
+				},
+				{
+					"name": "DB Blue 500",
+					"group": "Brand Colors",
+					"color": "#1455c0"
+				},
+				{
+					"name": "DB Blue 600",
+					"group": "Brand Colors",
+					"color": "#0c3992"
+				},
+				{
+					"name": "DB Blue 700",
+					"group": "Brand Colors",
+					"color": "#0a1e6e"
+				},
+				{
+					"name": "DB Blue 800",
+					"group": "Brand Colors",
+					"color": "#061350"
+				},
+				{
+					"name": "DB Cyan 100",
+					"group": "Brand Colors",
+					"color": "#e5faff"
+				},
+				{
+					"name": "DB Cyan 200",
+					"group": "Brand Colors",
+					"color": "#bbe6f8"
+				},
+				{
+					"name": "DB Cyan 300",
+					"group": "Brand Colors",
+					"color": "#84cfef"
+				},
+				{
+					"name": "DB Cyan 400",
+					"group": "Brand Colors",
+					"color": "#55b9e6"
+				},
+				{
+					"name": "DB Cyan 500",
+					"group": "Brand Colors",
+					"color": "#309fd1"
+				},
+				{
+					"name": "DB Cyan 600",
+					"group": "Brand Colors",
+					"color": "#0087b9"
+				},
+				{
+					"name": "DB Cyan 700",
+					"group": "Brand Colors",
+					"color": "#006a96"
+				},
+				{
+					"name": "DB Cyan 800",
+					"group": "Brand Colors",
+					"color": "#004b6d"
+				},
+				{
+					"name": "DB Turquoise 100",
+					"group": "Brand Colors",
+					"color": "#e3f5f4"
+				},
+				{
+					"name": "DB Turquoise 200",
+					"group": "Brand Colors",
+					"color": "#bee2e5"
+				},
+				{
+					"name": "DB Turquoise 300",
+					"group": "Brand Colors",
+					"color": "#83caca"
+				},
+				{
+					"name": "DB Turquoise 400",
+					"group": "Brand Colors",
+					"color": "#3cb5ae"
+				},
+				{
+					"name": "DB Turquoise 500",
+					"group": "Brand Colors",
+					"color": "#00a099"
+				},
+				{
+					"name": "DB Turquoise 600",
+					"group": "Brand Colors",
+					"color": "#008984"
+				},
+				{
+					"name": "DB Turquoise 700",
+					"group": "Brand Colors",
+					"color": "#006e6b"
+				},
+				{
+					"name": "DB Turquoise 800",
+					"group": "Brand Colors",
+					"color": "#005752"
+				},
+				{
+					"name": "DB Green 100",
+					"group": "Brand Colors",
+					"color": "#e2f3e5"
+				},
+				{
+					"name": "DB Green 200",
+					"group": "Brand Colors",
+					"color": "#bddbb9"
+				},
+				{
+					"name": "DB Green 300",
+					"group": "Brand Colors",
+					"color": "#8cbc80"
+				},
+				{
+					"name": "DB Green 400",
+					"group": "Brand Colors",
+					"color": "#66a558"
+				},
+				{
+					"name": "DB Green 500",
+					"group": "Brand Colors",
+					"color": "#408335"
+				},
+				{
+					"name": "DB Green 600",
+					"group": "Brand Colors",
+					"color": "#2a7230"
+				},
+				{
+					"name": "DB Green 700",
+					"group": "Brand Colors",
+					"color": "#165c27"
+				},
+				{
+					"name": "DB Green 800",
+					"group": "Brand Colors",
+					"color": "#154a26"
+				},
+				{
+					"name": "DB Light Green 100",
+					"group": "Brand Colors",
+					"color": "#ebf7dd"
+				},
+				{
+					"name": "DB Light Green 200",
+					"group": "Brand Colors",
+					"color": "#c9eb9e"
+				},
+				{
+					"name": "DB Light Green 300",
+					"group": "Brand Colors",
+					"color": "#9fd45f"
+				},
+				{
+					"name": "DB Light Green 400",
+					"group": "Brand Colors",
+					"color": "#78be14"
+				},
+				{
+					"name": "DB Light Green 500",
+					"group": "Brand Colors",
+					"color": "#63a615"
+				},
+				{
+					"name": "DB Light Green 600",
+					"group": "Brand Colors",
+					"color": "#508b1b"
+				},
+				{
+					"name": "DB Light Green 700",
+					"group": "Brand Colors",
+					"color": "#44741a"
+				},
+				{
+					"name": "DB Light Green 800",
+					"group": "Brand Colors",
+					"color": "#375f15"
+				},
+				{
+					"name": "DB Cool Gray 100",
+					"group": "Brand Colors",
+					"color": "#f0f3f5"
+				},
+				{
+					"name": "DB Cool Gray 200",
+					"group": "Brand Colors",
+					"color": "#d7dce1"
+				},
+				{
+					"name": "DB Cool Gray 300",
+					"group": "Brand Colors",
+					"color": "#afb4bb"
+				},
+				{
+					"name": "DB Cool Gray 400",
+					"group": "Brand Colors",
+					"color": "#878c96"
+				},
+				{
+					"name": "DB Cool Gray 500",
+					"group": "Brand Colors",
+					"color": "#646973"
+				},
+				{
+					"name": "DB Cool Gray 600",
+					"group": "Brand Colors",
+					"color": "#3c414b"
+				},
+				{
+					"name": "DB Cool Gray 700",
+					"group": "Brand Colors",
+					"color": "#282d37"
+				},
+				{
+					"name": "DB Cool Gray 800",
+					"group": "Brand Colors",
+					"color": "#131821"
+				},
+				{
+					"name": "ICE",
+					"group": "Transportation",
+					"color": "#282d37"
+				},
+				{
+					"name": "IC - EC",
+					"group": "Transportation",
+					"color": "#646973"
+				},
+				{
+					"name": "RE - RB",
+					"group": "Transportation",
+					"color": "#afb4bb"
+				},
+				{
+					"name": "S-Bahn",
+					"group": "Transportation",
+					"color": "#2a7230"
+				},
+				{
+					"name": "U-Bahn",
+					"group": "Transportation",
+					"color": "#1455c0"
+				},
+				{
+					"name": "Tram",
+					"group": "Transportation",
+					"color": "#a9455d"
+				},
+				{
+					"name": "Fernbus",
+					"group": "Transportation",
+					"color": "#e93e8f"
+				},
+				{
+					"name": "Bus",
+					"group": "Transportation",
+					"color": "#814997"
+				},
+				{
+					"name": "Schiff",
+					"group": "Transportation",
+					"color": "#309fd1"
+				},
+				{
+					"name": "Schnellboot",
+					"group": "Transportation",
+					"color": "#0087b9"
+				},
+				{
+					"name": "Flugzeug",
+					"group": "Transportation",
+					"color": "#00a099"
+				},
+				{
+					"name": "Taxi",
+					"group": "Transportation",
+					"color": "#ffd800"
+				},
+				{
+					"name": "Carsharing",
+					"group": "Transportation",
+					"color": "#f39200"
+				},
+				{
+					"name": "Auto",
+					"group": "Transportation",
+					"color": "#878c96"
+				},
+				{
+					"name": "E-Auto",
+					"group": "Transportation",
+					"color": "#408335"
+				},
+				{
+					"name": "Food Walk",
+					"group": "Transportation",
+					"color": "#d7dce1"
+				},
+				{
+					"name": "Fahrrad",
+					"group": "Transportation",
+					"color": "#63a615"
+				},
+				{
+					"name": "Bikesharing",
+					"group": "Transportation",
+					"color": "#c50014"
+				},
+				{
+					"name": "Fahrrad-Anhänger",
+					"group": "Transportation",
+					"color": "#878c96"
+				},
+				{
+					"name": "Roller",
+					"group": "Transportation",
+					"color": "#878c96"
+				},
+				{
+					"name": "Electric Scooter",
+					"group": "Transportation",
+					"color": "#878c96"
+				},
+				{
+					"name": "Essen & Trinken",
+					"group": "Point of Interest",
+					"color": "#f39200"
+				},
+				{
+					"name": "Einkaufen",
+					"group": "Point of Interest",
+					"color": "#814997"
+				},
+				{
+					"name": "Gesundheit",
+					"group": "Point of Interest",
+					"color": "#a9455d"
+				},
+				{
+					"name": "Kunst & Kultur",
+					"group": "Point of Interest",
+					"color": "#e93e8f"
+				},
+				{
+					"name": "Wissenswertes",
+					"group": "Point of Interest",
+					"color": "#858379"
+				},
+				{
+					"name": "Freizeit",
+					"group": "Point of Interest",
+					"color": "#408335"
+				},
+				{
+					"name": "Zivile & reli. Einrichtungen",
+					"group": "Point of Interest",
+					"color": "#00a099"
+				},
+				{
+					"name": "Dienstleistungen",
+					"group": "Point of Interest",
+					"color": "#309fd1"
+				},
+				{
+					"name": "DB Services & Einrichtung",
+					"group": "Point of Interest",
+					"color": "#0c3992"
+				},
+				{
+					"name": "Wegeleitung",
+					"group": "Point of Interest",
+					"color": "#3c414b"
+				},
+				{
+					"name": "Accessibiliy Blue",
+					"group": "Functional Colors",
+					"color": "#b2e1f5"
+				},
+				{
+					"name": "DB Success",
+					"group": "Functional Colors",
+					"color": "#508b1b"
+				},
+				{
+					"name": "DB Success Small Fontsize",
+					"group": "Functional Colors",
+					"color": "#467a18"
+				},
+				{
+					"name": "DB Error",
+					"group": "Functional Colors",
+					"color": "#ec0016"
+				},
+				{
+					"name": "DB Error Small Fontsize",
+					"group": "Functional Colors",
+					"color": "#db0014"
+				},
+				{
+					"name": "DB Warning",
+					"group": "Functional Colors",
+					"color": "#f75f00"
+				},
+				{
+					"name": "DB Warning Small Fontsize",
+					"group": "Functional Colors",
+					"color": "#cc4e00"
+				}
+]
+
+export{
+	colors
+}
--- src/global-functions.js
+++ src/global-functions.js
@@ -1,18 +1,85 @@
 
 
-export function hello(){
-	log("hello")
+import * as foundationColorsJson from './brandColors.js'
+
+
+var foundationColors = foundationColorsJson.colors;
+
+
+
+export function colorCheckBrandConformity (colorInput){
+
+  var result = foundationColors.find(color => color.color === colorInput.toLowerCase());
+  return result ? result : null;
 
 }
 
 
+
 export function hexAtoRgba(colorInput){
-var result = colorInput.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/i);
-return result ? {
-  r: parseInt(result[1], 16),
-  g: parseInt(result[2], 16),
-  b: parseInt(result[3], 16),
-  a: Math.round(1/255 * parseInt(result[4], 16) * 100) /100,
-  hex: '#' + result[1]+result[2]+result[3]
-} : null;
+
+
+  var result = colorInput.match(/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/i);
+
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+    a: Math.round(1/255 * parseInt(result[4], 16) * 100) /100,
+    hex: '#' + result[1]+result[2]+result[3],
+    color: colorCheckBrandConformity ( '#' + result[1]+result[2]+result[3])
+  } : null;
+
+}
+
+export function niceColorHexAlphaA(color){
+
+  return("Juri")
+}
+
+export function niceColorHexAlpha(color){
+  
+    var objFontColor = "";
+    var obFontColorObj = hexAtoRgba(color)
+    if(obFontColorObj.color){
+      objFontColor = "<td>"+obFontColorObj.color.name+",</br>Hex: "+obFontColorObj.hex+",</br>Alpha: "+obFontColorObj.a+"</td>";
+    } else {
+      objFontColor = "<td class='error'>Hex: "+obFontColorObj.hex+",</br>Alpha:"+obFontColorObj.a+"</td>";
+    }
+
+    return objFontColor;
+}
+
+
+
+
+export function saveToFile({ filenamePrefix, content: string, fileType }) {
+  // Configuring save panel
+  var savePanel = NSSavePanel.savePanel();
+  savePanel.allowedFileTypes = ["*"];
+  savePanel.nameFieldStringValue = `${filenamePrefix}`;
+
+  // Launching alert
+  var result = savePanel.runModal();
+  if (result == NSFileHandlingPanelOKButton) {
+    var path = savePanel.URL().path();
+    var success = string.writeToFile_atomically_encoding_error(path, true, NSUTF8StringEncoding, null);
+    var alert;
+
+    if (success) {
+     /* alert = createAlert({
+        text: 'The ' + FILETYPE.toUpperCase() + '-file is successfully saved to:\n `' + path + '`',
+        buttons: ['OK'],
+      });*/
+
+      //alert("Shared Color Palette JSON Exported!", "Styls Exprtet");
+    } else {
+     /* alert = createAlert({
+        text: `The file could not be saved.`,
+        buttons: ['OK'],
+      });*/
+    }
+    //alert("Shared Color Palette JSON Exported!", "Styls Not Exprtet");
+    //alert.runModal();
+  }
 }
